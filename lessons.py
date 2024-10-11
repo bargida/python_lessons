e53d3305814c3abb969ad4478be72401dcf82de8
bargida_tilyakova <144976090+bargida@users.noreply.github.com>
2024-10-11 23:27:06 +0500

    tasks of python
    Working with variables, string

---
+++ lessons.py
@@ -0,0 +1,26 @@
+# Working with variables
+greeting = "Hello world"
+print(greeting)
+message = "You shouldn't give up"
+print(message)
+message = "Try to study hard"
+print(message)
+subject = "Artificial Intelligence"
+print(subject)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+# Working with string
+street = "Baraka"
+district = "Marshal"
+region = "Andijan"
+print(f"{street} street, {district} district, {region} region")
+street = input("Enter your street\n")
+district = input("Enter your district\n")
+region = input("Enter your region\n")
+print(f"My street is {street}\n my district is {district}\n my region is {region}")
+
+# Working with numbers
