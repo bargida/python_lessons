43d1a92346b00d4f1e54c167e0dece4d9ec48fec
bargida_tilyakova <144976090+bargida@users.noreply.github.com>
2024-10-17 17:56:00 +0500

    Update lessons.py
    working with list is completed

--- lessons.py
+++ lessons.py
@@ -1,26 +1,76 @@
 # Working with variables
-greeting = "Hello world"
-print(greeting)
-message = "You shouldn't give up"
-print(message)
-message = "Try to study hard"
-print(message)
-subject = "Artificial Intelligence"
-print(subject)
-
-radius = 5
-pi = 3.14159
-aylana_yuzi = pi * radius**2
-print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+# greeting = "Hello world"
+# print(greeting)
+# message = "You shouldn't give up"
+# print(message)
+# message = "Try to study hard"
+# print(message)
+# subject = "Artificial Intelligence"
+# print(subject)
+#
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
 
 # Working with string
-street = "Baraka"
-district = "Marshal"
-region = "Andijan"
-print(f"{street} street, {district} district, {region} region")
-street = input("Enter your street\n")
-district = input("Enter your district\n")
-region = input("Enter your region\n")
-print(f"My street is {street}\n my district is {district}\n my region is {region}")
+# street = "Baraka"
+# district = "Marshal"
+# region = "Andijan"
+# print(f"{street} street, {district} district, {region} region")
+# street = input("Enter your street\n")
+# district = input("Enter your district\n")
+# region = input("Enter your region\n")
+# print(f"My street is {street}\n my district is {district}\n my region is {region}")
 
 # Working with numbers
+# num = int(input("Enter any number "))
+# num1 = num**2
+# num2 = num**3
+# print(f"{num} ning kvadarti {num1} ga teng,\n{num} ning kubi {num2} ga teng")
+
+# age = int(input("How old are you? "))
+# birth_year = 2024 - age
+# print(f" You are {birth_year} ")
+
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# print(f"{num1} + {num2} = {num1+num2} \n{num1} - {num2} = {num1-num2} \n{num1} * {num2} ={num1*num2} \n{num1} / {num2} {num1/num2}")
+
+#Working with lists
+
+# names = ['Manzura', 'Ziyoda', 'Munisa']
+# print("Salom", names[1], "what do your plan for today? ")
+# print("Salom", names[0], "Would you like to learn coding?")
+# print("Salom", names[2], "What kind of books do you like?")
+
+# numbers = [3, 5, 7, 12, -34]
+# print(numbers)
+# numbers[3] = numbers[1] + numbers[3]
+# numbers[0] = numbers[3] + numbers[4]
+# print(numbers)
+#
+# historic_persons = ['Amir Temur', "Alisher Nvoiy", "Bobur"]
+# modern_persons  = ["Ilon Mask", "Bill Geyts"]
+# print("I wanna talk to from historic people", historic_persons.pop(1), "and from modern people", modern_persons.pop(1))
+#
+# friends = []
+# friends.append('Sarvinoz')
+# friends.append('Dilnoza')
+# friends.append('Gulnoza')
+# friends.append('Mushtariy')
+# print(friends)
+# friends.remove('Gulnoza')
+# friends.remove('Mushtariy')
+# print(friends)
+# friends.append("Durdona")
+# friends.insert(2, "Kumush")
+# friends.insert(0, "Diyora")
+# print(friends)
+#
+# guests = []
+# guests.append(friends.pop(2))
+# guests.append(friends.pop(0))
+# print(f"Guests are{guests}")
+
+
